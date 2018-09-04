6709ff6b19f12f3f5a50014aad403387a48c4602
Michael Smith <msmithboise@gmail.com>
2018-09-04 14:31:29 -0600

    push code

---
+++ server/server-assets/routes/lists.js
@@ -0,0 +1,15 @@
+let router = require('express').Router()
+let Lists = require('../models/list')
+
+router.get('/:boardId', (req, res, next) => {
+    Lists.find({boardId: req.params.boardId})
+    .then(lists => {
+        res.send(lists)
+    })
+    .catch(err => {
+        console.log(err)
+        next()
+    })
+})
+
+
