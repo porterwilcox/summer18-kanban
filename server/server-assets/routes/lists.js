6c5f82ad07ca42349ada14a2d5dad83b1bd6b37a
Michael Smith <msmithboise@gmail.com>
2018-09-04 14:44:13 -0600

    list requests done

--- server/server-assets/routes/lists.js
+++ server/server-assets/routes/lists.js
@@ -2,14 +2,31 @@ let router = require('express').Router()
 let Lists = require('../models/list')
 
 router.get('/:boardId', (req, res, next) => {
-    Lists.find({boardId: req.params.boardId})
-    .then(lists => {
-        res.send(lists)
-    })
-    .catch(err => {
-        console.log(err)
-        next()
-    })
+    Lists.find({ boardId: req.params.boardId })
+        .then(lists => {
+            res.send(lists)
+        })
+        .catch(err => {
+            console.log(err)
+            next()
+        })
 })
 
+router.post('/', (req, res, next) => {
+    Lists.create(req.body)
+        .then(lists => {
+            res.send(lists)
+        })
+        .catch(err => {
+            console.log(err)
+            next()
+        })
+})
+
+router.delete('/:listId', (req, res, next) => {
+Lists.findByIdAndRemove(req.params.listId)
+.then(res => res.send({
+    message: 'Deleted!'
+}))
+})
 module.exports = router
