a74ae1383692cb0e0ce59d9a3af1ecde75155b53
Porter Wilcox <wilcox.porter@gmail.com>
2018-09-05 15:33:33 -0600

    comments bound to their parent task for render

--- client/src/components/Task.vue
+++ client/src/components/Task.vue
@@ -1,71 +1,74 @@
 <template>
-<div class="task">
-  <h3>{{taskData.title}}</h3>
-  <p>{{ taskData.timestamp | moment("ddd, h:mma") }}</p>
-  <div>
-    <div v-if="!showCommentForm">
-      <p @click="showCommentForm = !showCommentForm">add comment</p>
+  <div class="task">
+    <h3>{{taskData.title}}</h3>
+    <p>{{ taskData.timestamp | moment("ddd, h:mma") }}</p>
+    <div>
+      <div v-if="!showCommentForm">
+        <p @click="showCommentForm = !showCommentForm">add comment</p>
+      </div>
+      <div v-else>
+        <form @submit.prevent="addComment">
+          <input type="text" v-model="commentDescription" autofocus @focusout="showCommentForm = !showCommentForm">
+        </form>
+      </div>
     </div>
-    <div v-else>
-      <form @submit.prevent="addComment">
-        <input type="text" v-model="commentDescription" autofocus @focusout="showCommentForm = !showCommentForm">
-      </form>
+    <div v-for="(value, key) in comments" :key="key">
+      <div v-for="comment in value" :key="comment._id" v-if="comment.taskId == taskData._id">
+        {{comment.description}}
+      </div>
     </div>
+    <button @click="deleteTask">delete task</button>
   </div>
-  <div>
-    {{comments}}
-  </div>
-  <button @click="deleteTask">delete task</button>
-</div>
 </template>
 
 <script>
-export default {
-  name: "task",
-  props: ["taskData"],
-  data() {
-    return {
-      showCommentForm: false,
-      commentDescription: ""
-    };
-  },
-  computed: {
-    tasks() {
-      return this.$store.state.tasks[this.taskData.listId];
-    },
-    comments() {
-      return this.$store.state.comments;
-    }
-  },
-  methods: {
-    deleteTask() {
-      this.$store.dispatch("deleteTask", {
-        taskId: this.taskData._id,
-        listId: this.taskData.listId
-      });
-    },
-    addComment() {
-      let obj = {
-        description: this.commentDescription,
-        taskId: this.taskData._id,
-        timestamp: Date.now()
+  export default {
+    name: "task",
+    props: ["taskData"],
+    data() {
+      return {
+        showCommentForm: false,
+        commentDescription: ""
       };
-      this.$store.dispatch("addComment", obj);
-      this.commentDescription = "";
-      this.showCommentForm = false;
+    },
+    computed: {
+      tasks() {
+        return this.$store.state.tasks[this.taskData.listId];
+      },
+      comments() {
+        return this.$store.state.comments;
+      }
+    },
+    methods: {
+      deleteTask() {
+        this.$store.dispatch("deleteTask", {
+          taskId: this.taskData._id,
+          listId: this.taskData.listId
+        });
+      },
+      addComment() {
+        let obj = {
+          description: this.commentDescription,
+          taskId: this.taskData._id,
+          timestamp: Date.now()
+        };
+        this.$store.dispatch("addComment", obj);
+        this.commentDescription = "";
+        this.showCommentForm = false;
+      }
+    },
+    mounted() {
+      this.$store.dispatch("getComments", this.taskData._id);
     }
-  },
-  mounted() {
-    this.$store.dispatch("getComments", this.taskData._id);
-  }
-};
+  };
 </script>
 
 <style>
-.task {
-  background-color: red;
-}
-p {
-  cursor: pointer;
-}
+  .task {
+    background-color: red;
+  }
+
+  p {
+    cursor: pointer;
+  }
 </style>
