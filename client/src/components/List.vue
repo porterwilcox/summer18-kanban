8404953c3eaaf752aaff10a5fba9c5b9e225988d
Porter Wilcox <wilcox.porter@gmail.com>
2018-09-05 09:51:22 -0600

    made list component for scalability organization

--- client/src/components/List.vue
+++ client/src/components/List.vue
@@ -1,11 +1,52 @@
 <template>
-  
+<div>
+     <form @submit.prevent="addList">
+      <input type="text" required v-model="listTitle">
+   </form>
+     <div v-for="(value, key) in lists" :key="key">
+     <h1>
+     {{value.title}}
+     </h1>
+    <button @click="deleteList(value._id)">Delete</button>
+     </div>
+     <div>{{boardId}}</div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'List'
-}
+  name: "List",
+  data() {
+    return {
+      listTitle: ""
+    };
+  },
+  props: ["boardId"],
+  computed: {
+    lists() {
+      return this.$store.state.lists;
+    },
+    theBoardId() {
+      return this.boardId;
+    }
+  },
+  methods: {
+    addList() {
+      let obj = {
+        title: this.listTitle,
+        boardId: this.theBoardId
+      };
+      this.$store.dispatch("addList", obj);
+    },
+
+    deleteList(listId) {
+      this.$store.dispatch("deleteList", listId);
+    }
+  },
+  mounted() {
+    this.$store.dispatch("getLists", this.theBoardId);
+  }
+};
 </script>
 
 <style scoped>
--- client/src/views/Board.vue
+++ client/src/views/Board.vue
@@ -1,56 +1,26 @@
 <template>
   <div class="board">
-   <form @submit.prevent="addList">
-     <input type="text" required v-model="listTitle">
-
-   </form>
-   <div v-for="(value, key) in lists" :key="key">
-     <h1>
-     {{value.title}}
-     </h1>
-    <button @click="deleteList(value._id)">Delete</button>
-     
+  <lists v-bind:boardId="theBoardId" />
    </div>
-  </div>
 </template>
 
 <script>
+import Lists from "@/components/List"
 export default {
   name: "board",
-  data() {
-    return {
-      listTitle: ""
-    };
-  },
-  methods: {
-    addList() {
-      let obj = {
-        title: this.listTitle,
-        boardId: this.theBoardId
-      };
-      this.$store.dispatch("addList", obj);
-    },
-
-    deleteList(listId){
-   this.$store.dispatch("deleteList", listId);
-    }
-  },
   computed: {
     theBoardId() {
       return this.boardId;
-    },
-    lists() {
-      return this.$store.state.lists;
     }
+  },
+  components: {
+    Lists
   },
   created() {
     //blocks users not logged in
     if (!this.$store.state.user._id) {
       this.$router.push({ name: "login" });
     }
-  },
-  mounted() {
-    this.$store.dispatch("getLists", this.theBoardId);
   },
   props: ["boardId"]
 };
