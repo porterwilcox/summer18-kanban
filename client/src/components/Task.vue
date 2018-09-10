85a541555ba05aa27fbdbe743e5e680376b12c76
Porter Wilcox <wilcox.porter@gmail.com>
2018-09-10 11:36:44 -0600

    comment modal is linked to first task's id for an unknown reason

--- client/src/components/Task.vue
+++ client/src/components/Task.vue
@@ -1,53 +1,44 @@
 <template>
   <drag :transfer-data="{taskId: taskData._id, oldList: taskData.listId}" class="task">
-    <p class="task-title">{{taskData.title}}</p>
-    <p class="task-timestamp">{{ taskData.timestamp | timeFormat }}</p>
-    <div>
-
-      <!-- button trigger modal -->
-      
-          <i data-toggle="modal" data-target="#newModal" class="far fa-comment-dots"></i>
-
-      <!-- modal -->
-      <div class="modal fade" id="newModal" tabindex="-1" role="dialog" aria-labelledby="newModalTitle" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="newModalTitle">Comments</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              <form @submit.prevent="addComment">
-                <input placeholder="Add a comment" type="text" v-model="commentDescription" autofocus @focusout="showCommentForm = !showCommentForm">
-              </form>
-
-
-              
-              <div v-for="(value, key) in comments" :key="key" class="comments">
-                <div v-for="comment in value" :key="comment._id" v-if="comment.taskId == taskData._id">
-                  <p class="commentD">{{comment.description}}</p>
-                  <p>{{comment.timestamp | timeFormat}}</p>
-                  <i class="far fa-trash-alt" type="button" @click="deleteComment(comment._id)"></i>
+    {{taskData._id}}
+    <div class="task-details">
+      <p class="task-title">{{taskData.title}}</p>
+      <p class="task-timestamp">{{ taskData.timestamp | timeFormat }}</p>
+    </div>
+    <div class="task-buttons">
+      <div class="comment-bubble">      
+        <i @click="actualTaskId = taskData._id" data-toggle="modal" data-target="#newModal" class="far fa-comment-dots"></i>
+        <div class="modal fade" id="newModal" tabindex="-1" role="dialog" aria-labelledby="newModalTitle" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="newModalTitle">Comments</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <form @submit.prevent="addComment">
+                  <input placeholder="Add a comment" type="text" v-model="commentDescription" autofocus @focusout="showCommentForm = !showCommentForm">
+                </form>             
+                <div v-for="(value, key) in comments" :key="key" class="comments">
+                  <div v-for="comment in value" :key="comment._id" v-if="comment.taskId == taskData._id">
+                    {{comment.taskId}}
+                    <p class="commentD">{{comment.description}}</p>
+                    <p>{{comment.timestamp | timeFormat}}</p>
+                    <i class="far fa-trash-alt" type="button" @click="deleteComment(comment._id)"></i>
+                  </div>
                 </div>
               </div>
-
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-
-
-
-
+      <i class="far fa-trash-alt trashcan" type="button" @click="deleteTask"></i>
     </div>
-
-    <i class="far fa-trash-alt trashcan" type="button" @click="deleteTask"></i>
   </drag>
 </template>
 
@@ -60,7 +51,8 @@
     data() {
       return {
         showCommentForm: false,
-        commentDescription: ""
+        commentDescription: "",
+        actualTaskId: ''
       };
     },
     computed: {
@@ -87,6 +79,7 @@
           taskId: this.taskData._id,
           timestamp: Date.now()
         };
+        console.log(obj)
         this.$store.dispatch("addComment", obj);
         this.commentDescription = "";
         this.showCommentForm = false;
@@ -106,18 +99,6 @@
         };
         this.$store.dispatch("changeList", obj);
       }
-      // passTargetTask(transferData, nativeEvent) {
-      //   console.log(nativeEvent)
-      //   this.$emit("passTaskId", this.taskData._id);
-      // },
-      // initiateChange(transferData, nativeEvent) {
-      //   let obj = {
-      //     listId: this.listData._id,
-      //     oldList: this.oldListId,
-      //     taskId: this.taskId
-      //   };
-      //   this.$store.dispatch("changeList", obj);
-      // }
     },
     filters: {
       timeFormat: function (date) {
@@ -145,22 +126,41 @@
     border-style: solid;
     border-width: 1px;
     border-radius: 10%;
-    
-    
+    height: fit-content;
+    width: 18.5vw;
+    display: flex;
+    flex-flow: wrap row;    
+  }
+  .task-details {
+    display: flex;
+    flex-flow: wrap column;
+    width: 100%;
+    justify-content: flex-start;
+    align-items: flex-start;
   }
   .task-title{
     font-size: 18px;
-    padding: 3%;
     font-weight: bold;
+    margin-bottom: -10px;
+    margin-left: 5px;
   }
   .task-timestamp{
-    padding: 2%;
+    margin-bottom: -10px;
+    margin-left: 5px;
   }
-
+  .task-buttons {
+    display: flex;
+    flex-flow: wrap row;
+    width: 100%;
+    justify-content: flex-end;
+  }
+  .comment-bubble {
+    margin-right: 10px;
+    margin-top: 5px;
+  }
   .comments {
     background-color: #f9f9f9;
   }
-
   p {
     cursor: pointer;
   }
