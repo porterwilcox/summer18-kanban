8bde366c380ed8c7b278adfbbece63d7904a6d1c
Porter Wilcox <wilcox.porter@gmail.com>
2018-09-29 17:12:41 -0600

    forward slash

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 3000
 
 server.use(express.static(__dirname + '/../client/dist'))
 
-var whitelist = ['http://localhost:8080', 'https://trillo-kanban.herokuapp.com'];
+var whitelist = ['http://localhost:8080', 'https://trillo-kanban.herokuapp.com/'];
 var corsOptions = {
   origin: function (origin, callback) {
     var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
