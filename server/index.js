1a898ee40afe225e809e9132a41427b4cc3a9234
Porter Wilcox <wilcox.porter@gmail.com>
2018-09-29 16:52:41 -0600

    building

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ var express = require('express')
 var bp = require('body-parser')
 var server = express()
 var cors = require('cors')
-var port = 3000
+var port = process.env.PORT || 3000
 
-var whitelist = ['http://localhost:8080'];
+server.use(express.static(__dirname + '/../../client/dist'))
+
+var whitelist = ['http://localhost:8080', 'https://trillo-kanban.herokuapp.com'];
 var corsOptions = {
   origin: function (origin, callback) {
     var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
