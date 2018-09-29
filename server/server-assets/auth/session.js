1c5b4cded47c880ca4000131310d5f04578f3cb2
Porter Wilcox <wilcox.porter@gmail.com>
2018-09-29 16:29:55 -0600

    password check sync to return bool - cb

--- server/server-assets/auth/session.js
+++ server/server-assets/auth/session.js
@@ -12,7 +12,7 @@ store.on("error", function (err) {
 
 // @ts-ignore
 var session = expressSession({
-  secret: "What does secret even mean?", //CHANGE ME!!!!
+  secret: "PMKanban", //CHANGE ME!!!!
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 7 * 52 * 2,
   },
